9268b0ba414136e989630bd5105faa5eed5d6bf0
haegon <claudiopereiradev@gmail.com>
2022-01-12 00:22:00 +0000

    Updating

--- speech2text.py
+++ speech2text.py
@@ -11,31 +11,34 @@ files = sorted(os.listdir('parts/'))
 all_text = []
 newFile = "source/new-audio.wav"
 
+
 def transcriptFile():
-   for f in tqdm(files):
-      name = "parts/" + f
-      #Loading audio file
-      with sr.AudioFile(name) as source:
-         audio = r.record(source)
-      #Transcribe audio file
-      text = r.recognize_google(audio)
-      all_text.append(text)
+    for f in tqdm(files):
+        name = "parts/" + f
+        # Loading audio file
+        with sr.AudioFile(name) as source:
+            audio = r.record(source)
+        # Transcribe audio file
+        text = r.recognize_google(audio)
+        all_text.append(text)
 
-   transcript = ""
-   for i, t in enumerate(all_text):
-      total_seconds = i*30
-      m, s = divmod(total_seconds, 60)
-      h, m = divmod(m, 60)
+    transcript = ""
+    for i, t in enumerate(all_text):
+        total_seconds = i*30
+        m, s = divmod(total_seconds, 60)
+        h, m = divmod(m, 60)
 
-      transcript = transcript + "{:0>2d}:{:0>2d}:{:0>2d} {}\n".format(h, m, s, t)
+        transcript = transcript + \
+            "{:0>2d}:{:0>2d}:{:0>2d} {}\n".format(h, m, s, t)
 
-   print(transcript)
-   return transcript
+    print(transcript)
+    return transcript
+
 
 if __name__ == "__main__":
-   with open("transcript.txt", "w") as f:
-      f.write(transcriptFile())
-   if os.path.isfile(newFile):
-      os.remove(newFile)
-   else:
-      print("Error: %s file not found" % newFile)
+    with open("transcript.txt", "w") as f:
+        f.write(transcriptFile())
+    if os.path.isfile(newFile):
+        os.remove(newFile)
+    else:
+        print("Error: %s file not found" % newFile)
